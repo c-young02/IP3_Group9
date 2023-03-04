53c31f9ddcb1bd8ceedfe0e7eef084239f197112
c-young02 <callum.young02@hotmail.com>
2023-03-04 15:50:28 +0000

    Initialised project

---
+++ frontend/src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Switch, route, link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+	return <div className="App"></div>;
+}
+
+export default App;
