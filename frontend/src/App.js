dfa29d2b3a1c667eb0b0fde70d1855b0e658a33f
c-young02 <callum.young02@hotmail.com>
2023-03-06 22:54:49 +0000

    Started adding map

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,22 @@
-import React from 'react';
-import { Switch, route, link } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useState } from 'react';
+import ReactMapGL from 'react-map-gl';
 
-function App() {
-	return <div className="App"></div>;
+export default function App() {
+	const [viewport, setViewport] = useState({
+		latitude: 55.860916,
+		longitude: -4.251433,
+		width: '100%',
+		height: '100%',
+		zoom: 12,
+	});
+	return (
+		<div className="App">
+			<ReactMapGL
+				{...viewport}
+				mapboxAccessToken="pk.eyJ1IjoiYy15b3VuZzAyIiwiYSI6ImNsZXhjd2xqOTI5cHozeXAxbG02NndlNWUifQ.GcKdJYrL-O6qCKW1UK4dMQ"
+			>
+				Markers
+			</ReactMapGL>
+		</div>
+	);
 }
-
-export default App;
