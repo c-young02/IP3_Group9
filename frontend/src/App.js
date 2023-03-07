3057086afb0dcf593f3cf4024e0afd4060923714
c-young02 <callum.young02@hotmail.com>
2023-03-07 14:05:09 +0000

    set size to window

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ export default function App() {
 		<Map
 			mapboxAccessToken="pk.eyJ1IjoiYy15b3VuZzAyIiwiYSI6ImNsZXhjd2xqOTI5cHozeXAxbG02NndlNWUifQ.GcKdJYrL-O6qCKW1UK4dMQ" //todo make environment variable
 			initialViewState={{ latitude: 55.8668, longitude: -4.25, zoom: 12 }}
-			style={{ width: 1500, height: 800 }}
+			style={{ width: '100vw', height: '100vh' }}
 			mapStyle="mapbox://styles/mapbox/dark-v11"
 		/>
 	);
