aea4dbbe1f1fc6a958cc057bb60915708641f082
Logan Mclachlan <115151103+L0ganMclachlan@users.noreply.github.com>
2023-03-07 19:58:15 +0000

    Added files to handle asteroid apis
    Asteroid model is the model for all asteroid data.
    Asteroid processor retrieves api data and parses it into a asteroid model.
    Asteroid query holds info on a specific query that is used by the asteroid processor.

---
+++ backend/AsteroidModel.cs
@@ -0,0 +1,50 @@
+﻿// author(s): logan mclachlan
+
+using System.Collections.Generic;
+
+namespace NasaAPITest
+{
+    public class AsteroidModel
+    {
+        public Dictionary<string,List<Asteroid>> near_earth_objects { get; set; }
+    }
+
+    public class Asteroid
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public Estimated_Diameter estimated_diameter { get; set; }
+        public bool is_potentially_hazardous_asteroid { get; set; }
+        public Close_Approach_Data[] close_approach_data { get; set; }
+    }
+
+        public class Estimated_Diameter
+    {
+        public Meters meters { get; set; }
+    }
+
+
+    public class Meters
+    {
+        public float estimated_diameter_min { get; set; }
+        public float estimated_diameter_max { get; set; }
+    }
+
+    public class Close_Approach_Data
+    {
+        public string close_approach_date { get; set; }
+        public Relative_Velocity relative_velocity { get; set; }
+        public Miss_Distance miss_distance { get; set; }
+    }
+
+    public class Relative_Velocity
+    {
+        public string kilometers_per_second { get; set; }
+    }
+
+    public class Miss_Distance
+    {
+        public string lunar { get; set; }
+        public string kilometers { get; set; }
+    }
+}
---
+++ backend/AsteroidQuery.cs
@@ -0,0 +1,44 @@
+﻿// author(s): logan mclachlan
+
+using System;
+
+namespace NasaAPITest
+{
+    // stores info on an asteroid query and its results
+    public class AsteriodQuery
+    {
+        // sets the queries properties
+        public DateTime startDate { get; set; }
+        public DateTime endDate { get; set; }
+        public string id { get; set; }
+
+        // constructor, parameters are optional and have defualt values
+        public AsteriodQuery(DateTime? startDateIn = null,
+            DateTime? endDateIn = null,
+            string idIn = null)
+        {
+            // if no dates are given, set to today
+            // done this way to work around a limitation
+            if (startDateIn != null)
+            {
+                startDate = (DateTime)startDateIn;
+            }
+            else
+            {
+                startDate = DateTime.Today;
+            }
+
+            if (endDateIn != null)
+            {
+                endDate = (DateTime)endDateIn;
+            }
+            else
+            {
+                endDate = DateTime.Today;
+            }
+
+            // sets the id
+            id = idIn;
+        }
+    }
+}
