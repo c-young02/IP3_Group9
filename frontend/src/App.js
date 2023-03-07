310216e6720ec45ecfd2c3396734d83dd948e5cb
c-young02 <callum.young02@hotmail.com>
2023-03-07 13:39:51 +0000

    Changed coords to match the university

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,9 +5,9 @@ export default function App() {
 	return (
 		<Map
 			mapboxAccessToken="pk.eyJ1IjoiYy15b3VuZzAyIiwiYSI6ImNsZXhjd2xqOTI5cHozeXAxbG02NndlNWUifQ.GcKdJYrL-O6qCKW1UK4dMQ" //todo make environment variable
-			initialViewState={{ latitude: 55.860916, longitude: -4.251433, zoom: 12 }}
+			initialViewState={{ latitude: 55.8668, longitude: -4.25, zoom: 15 }}
 			style={{ width: 1000, height: 800 }}
-			mapStyle="mapbox://styles/mapbox/streets-v9"
+			mapStyle="mapbox://styles/mapbox/satellite-v9"
 		/>
 	);
 }
