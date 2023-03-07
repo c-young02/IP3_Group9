12e51147c90d5de7a376251b5ad90a2b1412c435
c-young02 <callum.young02@hotmail.com>
2023-03-07 14:02:22 +0000

    dark mode

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,13 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Map from 'react-map-gl';
 
 export default function App() {
 	return (
 		<Map
 			mapboxAccessToken="pk.eyJ1IjoiYy15b3VuZzAyIiwiYSI6ImNsZXhjd2xqOTI5cHozeXAxbG02NndlNWUifQ.GcKdJYrL-O6qCKW1UK4dMQ" //todo make environment variable
-			initialViewState={{ latitude: 55.8668, longitude: -4.25, zoom: 15 }}
-			style={{ width: 1000, height: 800 }}
-			mapStyle="mapbox://styles/mapbox/satellite-v9"
+			initialViewState={{ latitude: 55.8668, longitude: -4.25, zoom: 12 }}
+			style={{ width: 1500, height: 800 }}
+			mapStyle="mapbox://styles/mapbox/dark-v11"
 		/>
 	);
 }
