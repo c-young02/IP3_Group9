1e5330e88f68ba89e33e2ae434183f6b21e42883
Logan Mclachlan <115151103+L0ganMclachlan@users.noreply.github.com>
2023-03-08 13:27:59 +0000

    Update AsteroidQuery.cs

--- backend/AsteroidQuery.cs
+++ backend/AsteroidQuery.cs
@@ -4,10 +4,8 @@ using System;
 
 namespace NasaAPITest
 {
-    // stores info on an asteroid query and its results
     public class AsteriodQuery
     {
-        // sets the queries properties
         public DateTime startDate { get; set; }
         public DateTime endDate { get; set; }
         public string id { get; set; }
@@ -17,8 +15,7 @@ namespace NasaAPITest
             DateTime? endDateIn = null,
             string idIn = null)
         {
-            // if no dates are given, set to today
-            // done this way to work around a limitation
+            
             if (startDateIn != null)
             {
                 startDate = (DateTime)startDateIn;
@@ -37,7 +34,6 @@ namespace NasaAPITest
                 endDate = DateTime.Today;
             }
 
-            // sets the id
             id = idIn;
         }
     }
