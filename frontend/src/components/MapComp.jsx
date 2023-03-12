e5c70da914ad70cdb01efb8ae5f478c05788c869
c-young02 <callum.young02@hotmail.com>
2023-03-12 14:44:32 +0000

    Added more markers

--- frontend/src/components/MapComp.jsx
+++ frontend/src/components/MapComp.jsx
@@ -6,16 +6,21 @@ function MapComp() {
 			mapboxAccessToken="pk.eyJ1IjoiYy15b3VuZzAyIiwiYSI6ImNsZXhjd2xqOTI5cHozeXAxbG02NndlNWUifQ.GcKdJYrL-O6qCKW1UK4dMQ" //todo make environment variable
 			initialViewState={{ latitude: 55.8668, longitude: -4.25, zoom: 6 }}
 			style={{ width: '100vw', height: '100vh' }}
-			mapStyle="mapbox://styles/mapbox/satellite-v9"
-		>
-			<Marker latitude={55} longitude={-4} anchor="bottom">
+			mapStyle="mapbox://styles/c-young02/clf2kthzf006g01ln8gmkfiv0"
+		> (//creates markers on the map)
+			<Marker latitude={55} longitude={-4} anchor="center">
 				<button className="marker-btn">
 					<img src="..\images\snow.png" alt="Snow Icon" />
 				</button>
 			</Marker>
-			<Marker latitude={50} longitude={-4} anchor="bottom">
+			<Marker latitude={51} longitude={-2} anchor="center">
 				<button className="marker-btn">
-					<img src="..\images\fire.png" alt="Snow Icon" />
+					<img src="..\images\fire.png" alt="Fire Icon" />
+				</button>
+			</Marker>
+			<Marker latitude={37.751783} longitude={14.994604} anchor="center">
+				<button className="marker-btn">
+					<img src="..\images\volcano.png" alt="Volcano Icon" />
 				</button>
 			</Marker>
 		</Map>
