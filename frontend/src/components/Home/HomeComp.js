1bc7393b1687914ced4e3384807e28dd66f7b798
scamer300 <scamer300@caledonian.ac.uk>
2023-03-28 00:19:01 +0100

    added image onclick functionality

--- frontend/src/components/Home/HomeComp.js
+++ frontend/src/components/Home/HomeComp.js
@@ -1,4 +1,6 @@
 import { useEffect, useState } from 'react';
+import Button from 'react-bootstrap/Button'
+import Modal from 'react-bootstrap/Modal'
 
 function HomeComp() {
 
@@ -15,19 +17,41 @@ function HomeComp() {
 
   const [data, setData] = useState([]);
 
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
-      <div className="Home" class="container-fluid">
-      <div class="text-center">
-        <h2>Data Visualisation of NASA API's</h2>
-      </div>
-      <div class="text-center">
-        <img class="img-fluid" alt="" src={data.hdurl} width="600"></img>
-        <h3>{data.title}</h3>
-        <p class="h5">{data.explanation}</p>   
-      </div>
-    </div>
-  );
-    
+    <><div className="Home" class="container-fluid" height="100%" >
+        <div className="text-center">
+          <h2>
+            Welcome to "Project name", an interactive website showcasing NASA APIs. 
+            The collection of APIs were hand-picked to showcase the breadth of NASA's open-source work. 
+            To begin, select from the menu options.
+          </h2>
+        </div>
+        <div class="text-center">
+          <img class="img-fluid" alt="" src={data.url} width="900" onClick={handleShow} ></img>
+          <h4 class="text-center">
+            NASA Astronomy Picture of the Day. Click the image for a full description.
+          </h4>
+          <Modal show={show} onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>{data.title}</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>{data.explanation}</Modal.Body>
+          <Modal.Footer>
+            <Button variant="dark" onClick={handleClose}>
+              Close
+            </Button>
+          </Modal.Footer>
+          </Modal>
+        </div>
+      </div>  
+    </>
+
+);
+
 }
 
 export default HomeComp;
