072abe7600e3dacc3ca43d6a775fb16004221951
scamer300 <scamer300@caledonian.ac.uk>
2023-03-28 16:50:02 +0100

    adding meteor info grid

--- frontend/src/components/Meteors/MeteorsComp.js
+++ frontend/src/components/Meteors/MeteorsComp.js
@@ -1,15 +1,40 @@
-import Card from 'react-bootstrap/Card'
+import MeteorList from './MeteorsList';
+import MeteorDate from './MeteorDate';
+
 
 function MeteorsComp() {
   
 
   return(
-    <div style={{ margin: "2em"}}>
-      <div className="text-center">
-        <h1>Meteor Information</h1>
+    <div className="container" style={{ margin: "2em"}}>
+      <h1 className="text-center">Meteor Information</h1>
+      <div className="row pt-3">
+        <div class="col-sm btn-group" role="group">
+            <button class="btn btn-secondary">
+              List View
+            </button>
+            <button class="btn btn-secondary">
+              Graph View
+            </button>
+        </div>
+        <div class="col-sm text-center">
+          <MeteorDate />
+        </div>
+        <div class="col-sm">
+          <button class="btn btn-secondary btn-lg dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Dropdown button
+          </button>
+          <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+
+          </div>
+        </div>
       </div>
-      
-    </div>  
+
+      <div className="meteorsList">
+			  <MeteorList />
+		  </div>
+
+    </div>        
   )
 }
 
