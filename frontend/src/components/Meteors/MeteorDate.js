fe6b620113d43a4905d38ab8136be065acf8e1aa
scamer300 <scamer300@caledonian.ac.uk>
2023-03-28 16:49:20 +0100

    add date selector

---
+++ frontend/src/components/Meteors/MeteorDate.js
@@ -0,0 +1,17 @@
+import Form from 'react-bootstrap/Form';
+import { useState } from 'react';
+
+export default function MeteorDate() {
+    const [date, setDate] = useState(new Date());
+    return (
+      <div className="App">       
+        <Form.Control
+            type="date"
+            name="datepick"
+            placeholder="DateRange"
+            value={date}
+            onChange={(e) => setDate(e.target.value)}
+        />
+      </div>
+    );
+  }
