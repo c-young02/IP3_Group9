9a46fea825834df1aa406b2a48c86bee1c412783
scamer300 <scamer300@caledonian.ac.uk>
2023-04-18 13:40:33 +0100

    additional content and css changes

--- src/components/About/AboutComp.js
+++ src/components/About/AboutComp.js
@@ -4,35 +4,39 @@ function AboutComp() {
 	document.title = 'About Us';
 
 	const Members = [
-		{ name: 'Stacey', role: 'Frontend Developer & Project Manager' },
-		{ name: 'Callum', role: 'Frontend Developer & Designer' },
-		{ name: 'Logan', role: 'Backend Developer & Designer' },
-		{ name: 'Darren', role: 'Backend Developer' },
-		{ name: 'Lauren', role: 'Report Writer' },
-		{ name: 'Dionne', role: 'Report Writer' },
+		{ name: 'Stacey', role: 'Frontend Developer & Project Manager', course: 'Software Development', year: '3' },
+		{ name: 'Callum', role: 'Frontend Developer & Designer', course: 'Computing', year: '3' },
+		{ name: 'Logan', role: 'Backend Developer & Designer', course: 'Computing', year: '3' },
+		{ name: 'Darren', role: 'Backend Developer', course: 'Software Development', year: '3' },
+		{ name: 'Lauren', role: 'Report Writer', course: 'Software Development', year: '3' },
+		{ name: 'Dionne', role: 'Report Writer', course: 'Computing', year: '3' },
 	];
 
 	return (
+		<div className="About">
 		<div className="container row align-items-start">
 			<h1 className="display-1 text-center">About Us</h1>
 			<div className="row align-items-end px-5">
 				{Members?.map((member) => (
 					<div key={member.name} className="col-lg-4 pt-2">
-						<div className="card bg-dark shadow" style={{ width: '20rem' }}>
+						<div className="card bg-dark shadow" style={{ width: '21rem' }}>
 							<div className="card-header text-center">
 								<h3>{member.name}</h3>
 							</div>
 							<div className="card-body">
 								<h5 className="card-text">Role(s): {member.role}</h5>
+								<h5 className="card-text">Course: {member.course}</h5>
+								<h5 className="card-text">Year: {member.year}</h5>
 							</div>
 						</div>
 					</div>
 				))}
 			</div>
-			<a href="https://github.com/c-young02/IP3_Group9" className="text-center">
+			<a href="https://github.com/c-young02/IP3_Group9" className="text-center pt-3">
 				<AiOutlineGithub />
 				GitHub Repository
 			</a>
+		</div>
 		</div>
 	);
 }
