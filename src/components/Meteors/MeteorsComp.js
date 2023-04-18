68c5c67e1f1140aa88a94a499c64409a981ffea5
scamer300 <scamer300@caledonian.ac.uk>
2023-04-18 13:40:51 +0100

    css changes

--- src/components/Meteors/MeteorsComp.js
+++ src/components/Meteors/MeteorsComp.js
@@ -29,42 +29,44 @@ function MeteorsComp() {
 				<div className="row align-items-end pt-3">
 					{asteroid?.map((asteroid) => (
 						<div className="col-lg-3 pt-2" key={asteroid.id}>
-							<div className="card bg-dark" style={{ width: '17rem' }}>
+							<div className="card bg-dark" style={{ width: '18rem' }}>
 								<div className="card-header">
 									<h5>ID: {asteroid.id}</h5>
 									<h5>Name: {asteroid.name_limited}</h5>
 								</div>
 								<div className="card-body">
 									<h5 className="card-text">
-										Min Diameter:
+										Min Diameter (km):&nbsp;
 										{
 											asteroid.estimated_diameter.kilometers
-												.estimated_diameter_min
-										}
-										km
+												.estimated_diameter_min.toFixed(2)
+										}										
 									</h5>
 									<h5 className="card-text">
-										Max Diameter:
+										Max Diameter (km):&nbsp;
 										{
 											asteroid.estimated_diameter.kilometers
-												.estimated_diameter_max
+												.estimated_diameter_max.toFixed(2)
 										}
-										km
 									</h5>
 									<h5 className="card-text">
-										Velocity:
+										Velocity (km/s):<br></br>
 										{
 											asteroid.close_approach_data[0].relative_velocity
 												.kilometers_per_second
 										}
 									</h5>
 									<h5 className="card-text">
-										Closest Date:
-										{asteroid.close_approach_data[0].close_approach_date}
+										Closest Date:&nbsp;
+										{
+											asteroid.close_approach_data[0].close_approach_date
+										}
 									</h5>
 									<h5 className="card-text">
-										Miss Distance:
-										{asteroid.close_approach_data[0].miss_distance.lunar}
+										Miss Distance (LD):<br></br>
+										{
+											asteroid.close_approach_data[0].miss_distance.lunar
+										}
 									</h5>
 								</div>
 							</div>
